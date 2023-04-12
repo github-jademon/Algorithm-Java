5af50d359c8b014e22eb6eb43804d7572a21125b
jademon <j2python@gmail.com>
2023-04-12 11:13:54 +0900

    solve p2744

---
+++ src/p2744.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class p2744 {
+    // https://www.acmicpc.net/problem/2744
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String n = scanner.next();
+        String r = "";
+
+        for(char c:n.toCharArray()) {
+            if(Character.isLowerCase(c)) r+=Character.toUpperCase(c);
+            else r+= Character.toLowerCase(c);
+        }
+
+        System.out.println(r);
+    }
+}
