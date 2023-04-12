8ca61d0c29276a302d84450b0b837282ba1e52d6
jademon <j2python@gmail.com>
2023-04-12 14:03:50 +0900

    solve p3733

---
+++ src/p3733.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class p3733 {
+    // https://www.acmicpc.net/problem/3733
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n, s;
+
+        while(scanner.hasNext()) {
+            n = scanner.nextInt();
+            s = scanner.nextInt();
+            System.out.println(s/(n+1));
+        }
+    }
+}
