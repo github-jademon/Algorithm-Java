2e98d3942653b5b9e0ae1926547552c143d9401d
jademon <j2python@gmail.com>
2023-04-12 10:58:24 +0900

    solve p2438

---
+++ src/p2438.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class p2438 {
+    // https://www.acmicpc.net/problem/2438
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<=i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
