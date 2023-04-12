97ce0c531522e51a6f2b9b4ca4d72dad8553edd6
jademon <j2python@gmail.com>
2023-04-12 14:32:07 +0900

    add baekjoon package & move *

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
+    // https://www.acmicpc.net/problem/4999
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String n = scanner.next();
+        String s = scanner.next();
+
+        System.out.println(n.length()<s.length()?"no":"go");
     }
 }
