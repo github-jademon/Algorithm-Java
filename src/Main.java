a76a928d0d5bd58efdebcfacbcad9aaf786a18d2
jademon <j2python@gmail.com>
2023-04-12 14:33:41 +0900

    re main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
-    // https://www.acmicpc.net/problem/4999
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String n = scanner.next();
-        String s = scanner.next();
-
-        System.out.println(n.length()<s.length()?"no":"go");
     }
 }
