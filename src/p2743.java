42b661f6b30815ae95d87923f993ef166706dc18
jademon <j2python@gmail.com>
2023-04-12 11:05:42 +0900

    solve p2743

---
+++ src/p2743.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class p2743 {
+    // https://www.acmicpc.net/problem/2743
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String n = scanner.next();
+
+        System.out.println(n.length());
+    }
+}
