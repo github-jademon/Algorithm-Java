25787c999413f45de6f932491c33ca6c8931e86d
jademon <j2python@gmail.com>
2023-04-12 10:58:07 +0900

    solve p2420

---
+++ src/p2420.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class p2420 {
+    //https://www.acmicpc.net/problem/2420
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Long n = scanner.nextLong();
+        Long m = scanner.nextLong();
+
+        System.out.println(Math.abs(n-m));
+    }
+}
