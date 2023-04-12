0717caaf8562f5109ff657cf6bad7010dbb39d8a
jademon <j2python@gmail.com>
2023-04-12 12:23:04 +0900

    solve p3003

---
+++ src/p3003.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class p3003 {
+    // https://www.acmicpc.net/problem/3003
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] d = {1, 1, 2, 2, 2, 8};
+
+        for(int i=0; i<6; ++i) {
+            int n = scanner.nextInt();
+            System.out.print((d[i]-n)+" ");
+        }
+    }
+}
