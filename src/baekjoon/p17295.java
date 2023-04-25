5ddcecb260587b81d505ae943f56a1b7227b2dc4
jademon <j2python@gmail.com>
2023-04-26 00:01:56 +0900

    solve p17295

---
+++ src/baekjoon/p17295.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p17295 {
+    //https://www.acmicpc.net/problem/17295
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("Avengers: Endgame");
+    }
+}
