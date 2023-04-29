dfdefe64e350c0f4e3570678bd2e712e482e08bf
jademon <j2python@gmail.com>
2023-04-29 23:25:16 +0900

    solve p8545

---
+++ src/baekjoon/p8545.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p8545 {
+    //https://www.acmicpc.net/problem/8545
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer(scanner.next());
+        System.out.println(sb.reverse().toString());
+
+    }
+}
