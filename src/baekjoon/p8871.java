9618c677d5eba62dbd61ccc48bba174fa268c330
jademon <j2python@gmail.com>
2023-04-30 22:17:18 +0900

    solve p8871

---
+++ src/baekjoon/p8871.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p8871 {
+    //https://www.acmicpc.net/problem/8871
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(2*(n+1)+" "+3*(n+1));
+
+    }
+}
