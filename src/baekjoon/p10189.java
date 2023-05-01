c3b80ae5731c62ca0485cc0aed7818ee097b16ed
jademon <j2python@gmail.com>
2023-05-01 13:49:11 +0900

    solve p10189

---
+++ src/baekjoon/p10189.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class p10189 {
+    //https://www.acmicpc.net/problem/10189
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
