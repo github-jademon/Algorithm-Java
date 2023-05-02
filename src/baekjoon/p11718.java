a7ba799c5ab7617d79448b24b6fd8d691649071e
jademon <j2python@gmail.com>
2023-05-02 23:35:23 +0900

    solve p11718

---
+++ src/baekjoon/p11718.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p11718 {
+    //https://www.acmicpc.net/problem/11718
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
