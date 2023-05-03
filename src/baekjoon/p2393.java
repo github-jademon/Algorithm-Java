cf924cf82e48d84a843634d62c12b3320711b0e6
jademon <j2python@gmail.com>
2023-05-04 00:11:29 +0900

    solve p2393

---
+++ src/baekjoon/p2393.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.*;
+
+public class p2393 {
+    //https://www.acmicpc.net/problem/2393
+    public static void main(String[] args) throws IOException {
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+}
