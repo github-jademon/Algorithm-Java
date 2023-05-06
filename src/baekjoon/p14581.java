5841c3d48265bcea87eb913005b649655124e02d
jademon <j2python@gmail.com>
2023-05-07 02:00:10 +0900

    solve p14581

---
+++ src/baekjoon/p14581.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p14581 {
+    //https://www.acmicpc.net/problem/14581
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(":fan::fan::fan:\n" +
+                ":fan::"+scanner.next()+"::fan:\n" +
+                ":fan::fan::fan:");
+    }
+}
