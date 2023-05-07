1b72151c7b5bf86faaa68019e536e1541e0ebf35
jademon <j2python@gmail.com>
2023-05-08 01:42:08 +0900

    solve p15740

---
+++ src/baekjoon/p15740.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p15740 {
+    //https://www.acmicpc.net/problem/15740
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextBigInteger().add(scanner.nextBigInteger()));
+    }
+}
