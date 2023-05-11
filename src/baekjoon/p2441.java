0fa12be88e234f8af000ceb9bac2f82b52e91e72
jademon <j2python@gmail.com>
2023-05-11 19:44:13 +0900

    any

--- src/baekjoon/p2441.java
+++ src/baekjoon/p2441.java
@@ -11,7 +11,7 @@ public class p2441 {
             for(int j=0; j<i; ++j) {
                 System.out.print(" ");
             }
-            for(int j=0; j<5-i; ++j) {
+            for(int j=0; j<n-i; ++j) {
                 System.out.print("*");
             }
             System.out.println();
