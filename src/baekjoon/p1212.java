db25926b14284200750cc1ab8e8ec30e6e2df685
jademon <j2python@gmail.com>
2023-05-29 21:50:40 +0900

    solve p1212

---
+++ src/baekjoon/p1212.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p1212 {
+    //https://www.acmicpc.net/problem/1212
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextBigInteger(8).toString(2));
+    }
+}
