471864a7ac10eecb28a6916b139b7199ac68118b
jademon <j2python@gmail.com>
2023-06-02 00:03:15 +0900

    solve p26711

---
+++ src/baekjoon/p26711.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class p26711 {
+    // https://www.acmicpc.net/problem/26711
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextBigInteger().add(scanner.nextBigInteger()));
+    }
+}
