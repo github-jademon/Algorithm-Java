c4f56945a0cafed6577a1568670911df10b7c618
jademon <j2python@gmail.com>
2023-07-06 23:48:37 +0900

    solve p27323

---
+++ src/baekjoon/p27323.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p27323 {
+    //https://www.acmicpc.net/problem/27323
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        System.out.println(n*m);
+    }
+}
