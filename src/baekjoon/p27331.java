9a45afb8d92919f32ea028d861b1f971b0239900
jademon <j2python@gmail.com>
2023-07-14 21:42:10 +0900

    solve p27331

---
+++ src/baekjoon/p27331.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.*;
+
+public class p27331 {
+    //https://www.acmicpc.net/problem/27331
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+""+b);
+    }
+}
